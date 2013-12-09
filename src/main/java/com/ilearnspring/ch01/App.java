0fd716cdd818cc629dda01e6bee5cb27f22ea574
Rolly Ferolino <rferolino@jaredsoft.com>
2013-12-08 17:46:10 -0700

    initial commit

---
+++ src/main/java/com/ilearnspring/ch01/App.java
@@ -0,0 +1,13 @@
+package com.ilearnspring.ch01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
